f8303d1b8e990f417aa4a653081782c26515469b
Aaron Hinojosa <aaron@avenue6creative.com>
2019-01-14 20:17:47 -0600

    more work on rendering

--- main.cpp
+++ main.cpp
@@ -24,6 +24,38 @@ int main() {
         return 1;
     }
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    SDL_Texture *texture = SDL_CreateTexture(
+            renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC,
+            SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    if(renderer == NULL) {
+        cout << "Could not create renderer" << endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 3;
+    }
+
+    if(texture == NULL) {
+        cout << "Could not create texture" << endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 4;
+    }
+
+    // Uint32 is SDL type @ 32 bits
+    // each pixel is r,g,b,a -> 4 * 8 bits - 32 bits
+    Uint32 *buffer = new Uint32(SCREEN_WIDTH * SCREEN_HEIGHT);
+
+    // memset - set block of memory with particular value
+//    memset(buffer, 255, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+    SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
     bool quit = false;
 
     SDL_Event event;
@@ -45,7 +77,10 @@ int main() {
 
     }
 
-
+    // buffer is array so [] necessary
+    delete [] buffer;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
